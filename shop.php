560eb5bbbabc6b2f1bcb30afa04d16f12dfb53b2
hamza202 <k8668975@gmail.com>
2019-01-23 02:05:44 +0200

    First commit

---
+++ shop.php
@@ -0,0 +1,199 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="senjab"/>
+    <meta name="description" content="senjab"/>
+    <meta name="author" content="senjab"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>senjab</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo.png"/>
+
+    <!-- font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,500,500i,600,700,800,900|Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
+
+    <!-- Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/plugins-css.css"/>
+
+    <!-- Typography -->
+    <link rel="stylesheet" type="text/css" href="css/typography.css"/>
+
+    <!-- Shortcodes -->
+    <link rel="stylesheet" type="text/css" href="css/shortcodes/shortcodes.css"/>
+
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+
+    <link rel="stylesheet" href="css/custome.css">
+
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+
+    <!--=================================
+     preloader -->
+
+    <div id="pre-loader">
+        <img src="images/pre-loader/loader-01.svg" alt="">
+    </div>
+
+    <!--=================================
+     preloader -->
+
+
+    <!--=================================
+     header -->
+
+    <?php include 'header.php' ?>
+
+    <!--=================================
+     header -->
+
+
+    <!--=================================
+      banner -->
+    <section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}'
+             style="background-image: url(images/shop.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-name">
+                        <h1>Shop</h1>
+                        <p>We know the secret of your success</p>
+                    </div>
+                    <ul class="page-breadcrumb">
+                        <li><a href="#"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-double-right"></i>
+                        </li>
+                        <li><span>Shop </span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--=================================
+    page-title -->
+
+    <section class="page-section-ptb gray-bg">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12 col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="title-effect">Electronic Stores</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Amazon.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Aliexpress.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/EBay.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Amazon.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Aliexpress.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/EBay.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Amazon.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Aliexpress.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/EBay.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Amazon.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/Aliexpress.png" alt="">
+                    </a>
+                </div>
+                <div class="col-lg-3 col-md-4 col-6">
+                    <a href="#" class="card-centered">
+                        <img src="images/EBay.png" alt="">
+                    </a>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--footer -->
+
+    <?php include 'footer.php' ?>
+
+    <!--=================================
+     footer -->
+
+</div>
+
+<!--=================================
+ style-customizer  -->
+
+
+<!--=================================
+ style-customizer -->
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>TOP</span></a></div>
+
+<!--=================================
+ jquery -->
+
+<!-- jquery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<!-- plugins-jquery -->
+<script src="js/plugins-jquery.js"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = 'js/';</script>
+
+
+<!-- custom -->
+<script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
