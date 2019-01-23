76a2d3cf18a2dbbf38478d9692cdb0eca92ae104
hamza202 <k8668975@gmail.com>
2019-01-23 07:40:15 +0200

    First commit

---
+++ reviwes.php
@@ -0,0 +1,248 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="senjab"/>
+    <meta name="description" content="senjab"/>
+    <meta name="author" content="senjab"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>senjab</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo.png"/>
+
+    <!-- font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,500,500i,600,700,800,900|Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
+
+    <!-- Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/plugins-css.css"/>
+
+    <!-- Typography -->
+    <link rel="stylesheet" type="text/css" href="css/typography.css"/>
+
+    <!-- Shortcodes -->
+    <link rel="stylesheet" type="text/css" href="css/shortcodes/shortcodes.css"/>
+
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+
+    <link rel="stylesheet" href="css/custome.css">
+
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+
+    <!--=================================
+     preloader -->
+
+    <div id="pre-loader">
+        <img src="images/pre-loader/loader-01.svg" alt="">
+    </div>
+
+    <!--=================================
+     preloader -->
+
+
+    <!--=================================
+     header -->
+
+    <?php include 'header.php' ?>
+
+    <!--=================================
+     header -->
+
+
+    <!--=================================
+      banner -->
+    <section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}'
+             style="background-image: url(images/shop.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-name">
+                        <h1>Reviews</h1>
+                        <p>We know the secret of your success</p>
+                    </div>
+                    <ul class="page-breadcrumb">
+                        <li><a href="#"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-double-right"></i>
+                        </li>
+                        <li><span>Reviews </span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--=================================
+    page-title -->
+
+    <section class="page-section-ptb gray-bg reviews-section">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-12 col-md-12">
+                    <div class="section-title text-center">
+                        <h2 class="title-effect">Customer Reviews</h2>
+                    </div>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+                <div class="col-lg-6">
+                    <div class="testimonial bottom_pos">
+                        <div class="testimonial-avatar"> <img alt="" src="images/3.jpg"> </div>
+                        <div class="product-reviews-summary">
+                            <div class="rating-summary">
+                                <div class="rating-result" title="80%">
+                                    <span style="width:80%"></span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="testimonial-info"> This is a well put together template and the developer offers great support. I've been in contact with them and the response was prompt and everything has been taken care of.</div>
+                        <div class="author-info"> <strong>Ahmed - <span>Saudi Arabia</span></strong> </div>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+                <div class="col">
+                    <nav aria-label="Page navigation example">
+                        <ul class="pagination justify-content-center">
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Previous">
+                                    <span aria-hidden="true">&laquo;</span>
+                                    <span class="sr-only">Previous</span>
+                                </a>
+                            </li>
+                            <li class="page-item active"><a class="page-link" href="#">1</a></li>
+                            <li class="page-item"><a class="page-link" href="#">2</a></li>
+                            <li class="page-item"><a class="page-link" href="#">3</a></li>
+                            <li class="page-item">
+                                <a class="page-link" href="#" aria-label="Next">
+                                    <span aria-hidden="true">&raquo;</span>
+                                    <span class="sr-only">Next</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+
+    </section>
+    <!--footer -->
+
+    <?php include 'footer.php' ?>
+
+    <!--=================================
+     footer -->
+
+</div>
+
+<!--=================================
+ style-customizer  -->
+
+
+<!--=================================
+ style-customizer -->
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>TOP</span></a></div>
+
+<!--=================================
+ jquery -->
+
+<!-- jquery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<!-- plugins-jquery -->
+<script src="js/plugins-jquery.js"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = 'js/';</script>
+
+
+<!-- custom -->
+<script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
