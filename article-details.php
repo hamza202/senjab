62e959cee49e1447a43d0987c414abc9f8279358
hamza202 <k8668975@gmail.com>
2019-02-03 05:27:34 +0200

    First commit

---
+++ article-details.php
@@ -0,0 +1,165 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="keywords" content="senjab"/>
+    <meta name="description" content="senjab"/>
+    <meta name="author" content="senjab"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1"/>
+    <title>senjab</title>
+
+    <!-- Favicon -->
+    <link rel="shortcut icon" href="images/logo.png"/>
+
+    <!-- font -->
+    <link rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Montserrat:300,300i,400,500,500i,600,700,800,900|Poppins:200,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900">
+
+    <!-- Plugins -->
+    <link rel="stylesheet" type="text/css" href="css/plugins-css.css"/>
+
+    <!-- Typography -->
+    <link rel="stylesheet" type="text/css" href="css/typography.css"/>
+
+    <!-- Shortcodes -->
+    <link rel="stylesheet" type="text/css" href="css/shortcodes/shortcodes.css"/>
+
+    <!-- Style -->
+    <link rel="stylesheet" type="text/css" href="css/style.css"/>
+
+    <!-- Responsive -->
+    <link rel="stylesheet" type="text/css" href="css/responsive.css"/>
+
+    <link rel="stylesheet" href="css/custome.css">
+
+
+</head>
+
+<body>
+
+
+<div class="wrapper">
+
+    <!--=================================
+     preloader -->
+
+    <div id="pre-loader">
+        <img src="images/pre-loader/loader-01.svg" alt="">
+    </div>
+
+    <!--=================================
+     preloader -->
+
+
+    <!--=================================
+     header -->
+
+    <?php include 'header.php' ?>
+
+    <!--=================================
+     header -->
+
+
+    <!--=================================
+      banner -->
+    <section class="page-title bg-overlay-black-60 parallax" data-jarallax='{"speed": 0.6}'
+             style="background-image: url(images/articals.jpg);">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="page-title-name">
+                        <h1>Article details</h1>
+                    </div>
+                    <ul class="page-breadcrumb">
+                        <li><a href="#"><i class="fa fa-home"></i> Home</a> <i class="fa fa-angle-double-right"></i>
+                        </li>
+                        <li><a href="article.php"> Article</a> <i class="fa fa-angle-double-right"></i>
+                        </li>
+                        <li><span>Article details </span></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <!--=================================
+    page-title -->
+
+    <section class="blog white-bg page-section-ptb">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="blog-entry">
+                        <div class="blog-detail">
+                            <div class="entry-title mb-10">
+                                <h4 class="text-capitalize">test title</h4>
+                            </div>
+                            <div class="entry-meta mb-10">
+                                <ul>
+                                    <li><a href="#"><i class="fa fa-calendar-o"></i> 12 Aug 2018</a></li>
+                                </ul>
+                            </div>
+                            <div class="entry-content">
+                                <p class="text-justify">Consectetur, assumenda provident this exercise dolor sit amet, consectetur
+                                    adipisicing elit. Quae laboriosam sunt hic perspiciatis, asperiores mollitia
+                                    excepturi voluptatibus sequi nostrum ipsam veniam omnis nihil! A ea maiores
+                                    corporis. this exercise dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                    tempor incididunt ut labore et dolore magna aliqua.
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus dignissimos in
+                                    magnam necessitatibus nulla quo sit temporibus. A aliquid, assumenda autem
+                                    inventore, mollitia neque quas rem ut veniam vero voluptatem?
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium adipisci alias
+                                    asperiores aspernatur consequatur deserunt distinctio excepturi explicabo ipsam
+                                    ipsum, iusto perferendis reiciendis reprehenderit ut vitae! Cumque eveniet inventore
+                                    quod!
+                                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. A ab alias aspernatur
+                                    commodi cum eius error et maiores numquam, odit officiis porro quae repellat sed
+                                    sint soluta tempore veniam voluptatibus?
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!--=================================
+    footer -->
+    <?php include 'footer.php' ?>
+
+    <!--=================================
+     footer -->
+
+</div>
+
+<!--=================================
+ style-customizer  -->
+
+
+<!--=================================
+ style-customizer -->
+
+<div id="back-to-top"><a class="top arrow" href="#top"><i class="fa fa-angle-up"></i> <span>TOP</span></a></div>
+
+<!--=================================
+ jquery -->
+
+<!-- jquery -->
+<script src="js/jquery-3.3.1.min.js"></script>
+
+<!-- plugins-jquery -->
+<script src="js/plugins-jquery.js"></script>
+
+<!-- plugin_path -->
+<script>var plugin_path = 'js/';</script>
+
+
+<!-- custom -->
+<script src="js/custom.js"></script>
+
+
+</body>
+
+</html>
